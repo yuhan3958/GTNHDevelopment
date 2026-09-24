530b45dc8c44aa8dc0812ca94ea6c690f5392cab
yuhan8954 <yuhan8954@gmail.com>
2026-09-24 16:33:26 +0900

    bug fix

--- src/main/kotlin/dev/gtnh/intellij/project/GtnhFirstRunActivity.kt
+++ src/main/kotlin/dev/gtnh/intellij/project/GtnhFirstRunActivity.kt
@@ -3,13 +3,14 @@ package dev.gtnh.intellij.project
 import com.intellij.notification.NotificationAction
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.wm.ToolWindowManager
 import dev.gtnh.intellij.settings.GtnhSettingsState
 
-class GtnhFirstRunActivity : StartupActivity.DumbAware {
-    override fun runActivity(project: Project) {
+class GtnhFirstRunActivity : ProjectActivity, DumbAware {
+    override suspend fun execute(project: Project) {
         if (!GtnhSettingsState.getInstance().state.firstRunNotifications) return
         val state = project.getService(GtnhFirstRunState::class.java)
         if (state.shown || !GtnhProjectDetector.isGtnhProject(project)) return
